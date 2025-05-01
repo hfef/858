5eedc1fad0a6ccc6044b5875b7e5222991748683
hfef <121530967+hfef@users.noreply.github.com>
2025-05-01 04:58:00 +0000

    87

--- 78.py
+++ 78.py
@@ -50,6 +50,6 @@ def download_and_delete_image(url, save_path, num_downloads, delay):
 # 使用示例  
 save_path = "daia"  # 你需要提供一个有效的保存路径  
 url = "http://pmt823bd6.pic46.websiteonline.cn/upload/ym.png"  # 示例URL  
-num_downloads = 444 # 控制下载次数  
+num_downloads= 444 # 控制下载次数  
 delay = 1  # 指定下载间隔时间（秒）  
 download_and_delete_image(url, save_path, num_downloads, delay)
